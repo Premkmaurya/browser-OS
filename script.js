1e14f58e766e4c0b3ff1644d75e9b7855e664357
unknown <premkmaurya123456@gmail.com>
2025-07-02 14:08:56 +0530

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 document.addEventListener('contextmenu', event => event.preventDefault());
 
-let desktopArea = document.querySelector('main');
+let main = document.querySelector('main');
 let searchIcon = document.getElementById('search-icon');
 let searchInput = document.getElementById('search-bar');
 let dateDisplay = document.querySelector('.date');
@@ -8,7 +8,10 @@ let timeDisplay = document.querySelector('.hours');
 let contextMenu = document.querySelector('.right-click');
 let newFolderButton = document.querySelector('.new');
 let createFolderMenu = document.querySelector('.folder');
-let createShortcutButton = document.querySelector('.shortcut');
+let createShortcutFolderButton = document.querySelector('.shortcut');
+let createShortcutTextButton = document.querySelector('.shortcut-2');
+let volumeRange = document.querySelector('.volumeRange');
+let volumeIcon = document.querySelector('.volume-icon');
 
 
 
@@ -61,6 +64,11 @@ document.addEventListener('click', e => {
         contextMenu.classList.remove('visible');
         createFolderMenu.classList.remove('visible');
     }
+     if (!volumeRange.contains(e.target) && !volumeIcon.contains(e.target)) {
+        volumeRange.style.opacity = '0';
+    }
+    console.log(e.target);
+    
 })
 
 
@@ -94,35 +102,41 @@ function showCreateAt(x, y) {
     createFolderMenu.classList.add('visible');
 }
 
-createShortcutButton.addEventListener('click',()=>{
+createShortcutFolderButton.addEventListener('click', () => {
     createShortcut('New Folder');
+    createFolderMenu.classList.remove('visible');
+    contextMenu.classList.remove('visible');
+})
+
+createShortcutTextButton.addEventListener('click', () => {
+    createTextShortcut('new text document');
     createFolderMenu.classList.remove('visible');
     contextMenu.classList.remove('visible');
 })
 let createCount = 0;
 
-let currentX = 20;
-let currentY = 20;
+let currentX = 3;
+let currentY = 2;
 let stepY = 80;
 let stepX = 100;
-let maxY = 593;
+let maxY = 520;
 
-function nextPosition(){
+function nextPosition() {
     const position = {
         top: currentY,
         left: currentX
     }
     currentY += stepY;
-    if(currentY > maxY){
-        currentY = 20;
+    if (currentY > maxY) {
+        currentY = 2;
         currentX += stepX;
     }
     return position;
 }
 
-function createShortcut(name){
+function createShortcut(name) {
     const folder = document.createElement('div');
-    const {top,left} = nextPosition();
+    const { top, left } = nextPosition();
     folder.style.position = 'absolute';
     folder.style.left = `${left}px`;
     folder.style.color = 'white';
@@ -141,23 +155,55 @@ function createShortcut(name){
         <div class="folder-name">${name}</div>
     `;
     makedraggble(folder);
-    desktopArea.appendChild(folder);
+    main.appendChild(folder);
+    createCount++;
+}
+
+function createTextShortcut(name) {
+    const folder = document.createElement('div');
+    const { top, left } = nextPosition();
+    folder.style.position = 'absolute';
+    folder.style.left = `${left}px`;
+    folder.style.color = 'white';
+    folder.style.display = 'flex';
+    folder.style.top = `${top}px`;
+    folder.style.flexDirection = 'column';
+    folder.style.alignItems = 'center';
+    folder.style.justifyContent = 'center';
+    folder.style.fontSize = '13px';
+    folder.style.textAlign = 'center';
+    folder.style.width = '100px';
+    folder.style.height = '100px';
+    folder.style.zIndex = '10660';
+    folder.innerHTML = `
+        <img class="fa-folder" src="https://img.icons8.com/?size=256&id=21073&format=png" >
+        <div class="folder-name">${name}</div>
+    `;
+    makedraggble(folder);
+    main.appendChild(folder);
     createCount++;
 }
 
 function makedraggble(el) {
     el.onmousedown = function (e) {
-        e.preventDefault(); 
+        e.preventDefault();
         let offSetX = e.clientX - el.getBoundingClientRect().left;
-        let offSetY = e.clientY - el.getBoundingClientRect().top;   
-    function onMouseMove(ev) {
-        el.style.left = ev.clientX - offSetX + 'px';
-        el.style.top = ev.clientY - offSetY + 'px';
+        let offSetY = e.clientY - el.getBoundingClientRect().top;
+        function onMouseMove(ev) {
+            el.style.left = ev.clientX - offSetX + 'px';
+            el.style.top = ev.clientY - offSetY + 'px';
+        }
+        function onMouseUp() {
+            document.removeEventListener('mousemove', onMouseMove);
+            document.removeEventListener('mouseup', onMouseUp);
+        }
+        document.addEventListener('mousemove', onMouseMove);
+        document.addEventListener('mouseup', onMouseUp)
     }
-    function onMouseUp() {
-        document.removeEventListener('mousemove', onMouseMove);
-        document.removeEventListener('mouseup', onMouseUp);
-    }
-    document.addEventListener('mousemove', onMouseMove);
-    document.addEventListener('mouseup', onMouseUp)
-}}
+}
+
+volumeIcon.addEventListener('click',()=>{
+    if(volumeRange.style.opacity === '1'){
+        volumeRange.style.opacity = '0';
+    } else volumeRange.style.opacity = '1';
+})
